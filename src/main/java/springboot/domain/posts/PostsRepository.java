874b03f2d123022f44a8242e15ba639cac043aa1
park <tmdemr99@gmail.com>
2021-03-04 21:11:39 +0900

    Posts 테스트 클래스 추가 application.properties 추가

---
+++ src/main/java/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,10 @@
+package springboot.domain.posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+//DB Layer 접근자, <Entity 클래스, PK타입>을 상속하면 기본적인 CRUD 메소드가 자동으로 생성됨.
+//Entity 클래스와 Entity Repository는 함께 위치해야 한다.
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
